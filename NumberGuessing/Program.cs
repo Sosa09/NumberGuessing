640b25a61dfc0c15ba4f537bf1961c2a029847a8
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-07-09 20:47:16 +0200

    new update
    added 2 const
    OFFFIVE holds the value 5 needed to check if the actuall guess is 5 off
    
    TOTAL_CHANCES which holds the total chances before gameover

--- NumberGuessing/Program.cs
+++ NumberGuessing/Program.cs
@@ -7,8 +7,8 @@
             
             //Instantiating the Random class object
             Random random = new Random();
+            int TOTAL_CHANCES = 10;
 
-    
             //Introduction Text
             Console.WriteLine("Hello, World!, Let's play a game.\n" +
                               "The computer will generate a random nr from 1-100. \n" +
@@ -18,15 +18,16 @@
             {
                 //decalre the game values
                 int randomNr = random.Next(1, 100);
-                int chancesLeft= 10;
+                int chancesLeft= TOTAL_CHANCES;
+                int OFFFIVE = 5;
                 bool nrIsGuessed = false;
 
                 while (chancesLeft > 0 && nrIsGuessed == false) 
                 {
 
-
-                    Console.WriteLine($"Chances left: {chancesLeft}");
-                    Console.WriteLine("Choose a nr from 1 - 100: ");
+                     
+                    Console.WriteLine($"Chances left: {chancesLeft}");//Display chances left to the end user
+                    Console.WriteLine("Choose a nr from 1 - 100: "); //Ask user to choose  nr from 1 to 100
                     
                     int.TryParse(Console.ReadLine(), out int userInput); //Reading user inpyt and parsing into int at the same time as well as avoiding system to crash if userinput is not nr or empty
 
@@ -38,24 +39,20 @@
                     else if (randomNr == userInput)
                         nrIsGuessed=true;
 
+                   
                     //Checking if the userinput is close to the random nr
-                    if (userInput - 5 == randomNr || userInput + 5 == randomNr)
-                        Console.WriteLine("You're close though!");
+                    if (userInput - OFFFIVE == randomNr || userInput + OFFFIVE == randomNr)
+                        Console.WriteLine("You're close!");
 
                     chancesLeft--; //decrement chancesLeft
 
                 }
 
                 if (nrIsGuessed)
-                {
                     Console.WriteLine("Great you found it let's play again.");
-            
-                }
                 else
-                {
                     Console.WriteLine("\"You lost loser :)!");
 
-                }
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadKey();
                 Console.Clear();
